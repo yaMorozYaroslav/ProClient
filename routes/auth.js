8c00d4bbe2e2a5fb31c5272d5f3d64effcef890d
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-01-18 05:52:22 +0100

    Yar

--- routes/auth.js
+++ routes/auth.js
@@ -1,27 +1,49 @@
 const router = require('express').Router();
 const User = require('../models/User');
-const Joi = require('@hapi/joi');
-const schema = Joi.object({
-  name: Joi.string() .min(6) .required(),
-  email: Joi.string() .min(6) .required() .email(),
-  password: Joi.string() .min(6) .required() });
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const {registerValidation,loginValidation}=
+require('../validation');
+
 router.post('/register',async (req, res)=>{
-	const {error} =
-	schema.validate(req.body);
+	const{error}=registerValidation(req.body);
 	if(error)return res.status(400).send(
-		error.details[0].message);
+    error.details[0].message);
+  const emailExist = await User.findOne({
+  	email: req.body.email});
+  if(emailExist)return res.status(400).send(
+  	'Email already exists');
+  	const salt = await bcrypt.genSalt(10);
+const hashedPassword = 
+   await bcrypt.hash(req.body.password, salt);
+
 	const user = new User({
 		name: req.body.name,
 		email: req.body.email,
-		password: req.body.password
+		password: hashedPassword
 	});
 	try{
 		const savedUser = await user.save();
-		res.send(savedUser);
+		res.send({user: user._id});
 	}catch(err){
 		res.status(400).send(err);
 	}
-	
 });
-
+router.post('/login',async(req,res)=>{
+	const{error}=loginValidation(req.body);
+	if(error)return res.status(400).send(
+    error.details[0].message);
+  const user = await User.findOne({
+  	email:req.body.email
+  });
+  if (!user)return res.status(400).send(
+  	'Email isnt found');
+  const validPass = await bcrypt.compare(req.body.password, user.password);
+  if(!validPass)return res.status(400)
+  	.send('Invalid password');
+  const token = jwt.sign({_id: user._id},
+  	            config.get('jwtSecret'));
+  	res.header('auth-token', token).send(token);
+  });
 module.exports = router;
