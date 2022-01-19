77c82c34b350cf0cb2fb64307c5f12b4c9bada3e
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-01-19 05:51:33 +0100

    Yaro

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ mongoose.connect(uri)
 .then(()=>console.log('MongoDB Connected'))
 .catch(err=>console.log(err));
 const authRoute = require('./routes/auth');
+const postRoute = require('./routes/post')
 app.use('/api/user', authRoute);
+app.use('/api/post', postRoute);
 app.listen(3000, ()=>
 	console.log('Running Server'));
