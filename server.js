90d756d243ac2fc3a1610de2e44b9691c0bb6d30
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-02-15 21:38:37 +0100

    2

--- server.js
+++ server.js
@@ -7,22 +7,22 @@ const users = require('./routes/api/users');
 const auth = require('./routes/api/auth');
 const app = express();
 app.use(bodyParser.json());
-const config = require('config');
-//require('dotenv').config();
-const uri = config.get('mongoURI');
+//const config = require('config');
+require('dotenv').config();
+const uri = process.env.ATLAS_URI;
 mongoose.connect(uri)
 .then(()=>console.log('MongoDB Connected'))
 .catch(err => console.log(err));
 app.use('/api/items', items);
 app.use('/api/users', users);
 app.use('/api/auth', auth);
-if(config.get('node') ==='production'){
+if(process.env.NODE_ENV==='production'){
   app.use(express.static('client/build'));
   app.get('*', (req,res)=>{
     res.sendFile(path.resolve(
   __dirname, 'client', 'buildi','index.html'));
   });
 }
-const port = config.get('port') || 5000;
+const port = process.env.PORT || 5000;
 app.listen(port,()=>console.log
   (`Server started on port ${port}`));
