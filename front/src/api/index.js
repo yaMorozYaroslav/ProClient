22f4b55052c5f1ae4c43e53ed6f7569b07e64d48
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-05-08 21:35:22 +0200

    564

--- front/src/api/index.js
+++
@@ -1,5 +0,0 @@
-import axios from 'axios'
-
-const baseUrl = 'https://itemauthback.herokuapp.com'
-
-export const fetchItems =()=>axios.get(`${baseUrl}/api/items`)
---
+++ front/src/reducers/post.js
@@ -0,0 +1,14 @@
+import {GET_ITEMS, ADD_ITEM, DELETE} from '../constants/actionTypes'
+
+const func = (items=[], action) => {
+	switch(action.type){
+		case GET_ITEMS:
+		     return action.payload
+		case ADD_ITEM:
+		     return [...items, action.payload]
+		case DELETE:
+		     return items.filter((post)=>post._id !== action.payload)
+		default:
+		     return items
+	}
+}
