0304145eb3b663e057703ed77385bbf1c8a40bd7
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-05-10 21:03:45 +0200

    867

--- front/src/Form.js
+++ front/src/Form.js
@@ -4,5 +4,24 @@ import {useDispatch, useSelector} from 'react-redux'
 import {createPost} from '../actions/itemAct'
 
 export const Form = ()=> {
-	const [postData, setPostData] = React.useState({})
+	const dispatch = useDispatch()
+	const [postData, setPostData] = React.useState({name: ''})
+	const submit =(e)=>{
+		e.preventDefault()
+	if(postData.name){
+		dispatch(createPost(postData))
+		clear()
+	}
+
+	}
+	const clear =()=> {
+		setPostData({name: ''})
+	}
+	return(
+      <section><form>
+      <label htmlFor="title">title:</label>
+      <textarea id='postName' name='postName' value={memoData.title}
+                onChange={(e)=>setMemoData({...memoData, title: e.target.value})} />
+      </form></section>
+		)
 }
