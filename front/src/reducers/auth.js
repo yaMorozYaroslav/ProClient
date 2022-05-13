04e826fcf4cb241632019fc803d275df77be931a
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-05-13 22:26:31 +0200

    453

--- front/src/reducers/auth.js
+++ front/src/reducers/auth.js
@@ -1,15 +1,30 @@
 import {AUTH, LOGOUT} from '../constants/actionTypes'
 
-const authReducer = (state = {authData: null}, action) => {
+const initialState = {
+	token: localStorage.getItem('profile'),
+	isAuthenticated: null,
+	isLodaing: false,
+	user: null
+}
+export default function authReducer(state=initialState, action){
 	switch(action.type){
-	 case AUTH:
-        localStorage.setItem('profile', JSON.stringify({...action?.data}))
-     return {...state, authData: action?.data}
-     case LOGOUT:
-       localStorage.clear()
-       return {...state, authData: null}
-		default:
-		   return state
+		case AUTH:
+		   localStorage.setItem('profile', action.payload.token)
+		   return{
+		   	...state,
+		   	...action.payload,
+		   	isAuthenticated: true,
+		   	isLoading: false
+		   }
+		case LOGOUT:
+		   localStorage.removeItem('profile')
+		  return{
+		  	...state,
+		  	token: null,
+		  	user: null,
+		  	isAuthenticated: false,
+		  	isLoading: false
+		  }
+		  default: return state
 	}
 }
-export default authReducer
