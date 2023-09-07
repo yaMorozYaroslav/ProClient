a43e739b1ee7e9e794f4027fe2525801c75a4ec8
Yaroslav Moroz <ya.moroz.yaroslav@gmail.com>
2023-09-06 23:55:19 -0300

    200

--- back/index.js
+++ back/index.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import { MongoClient, ServerApiVersion } from "mongodb"
 
 import itemRoutes from './routes/items.js'
 import userRoutes from './routes/user.js'
@@ -20,7 +21,33 @@ app.get('/', (req,res)=>{
 	res.send('Hello to Items API')
 })
 const PORT = process.env.PORT||5000
-
+/*mongoose.set('strictQuery', false)
 mongoose.connect(process.env.CONNECTION_URL)
              .then(()=>app.listen(PORT, ()=>console.log(`Running on ${PORT}`)))
              .catch((error)=>console.log(error.message))
+*/
+
+const uri = "mongodb+srv://Yaroslav:HorHor37@cluster0.tlfsw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+export const client = new MongoClient(uri,  {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    }
+);
+async function run() {
+  try {
+    // Connect the client to the server (optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+app.listen(PORT, ()=>console.log(`Running on ${PORT}`))
