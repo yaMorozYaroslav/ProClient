93e356d03dca2df8f2e1f5ad13281419c896f8c0
Yaroslav Moroz <ya.moroz.yaroslav@gmail.com>
2023-09-09 21:12:34 -0300

    26

--- back/ctrls/items.js
+++ back/ctrls/items.js
@@ -1,23 +1,12 @@
 import express from 'express'
-//import Item from '../models/Item.js'
 import db from "../conn.js";
 import { ObjectId } from "mongodb";
 
-//const router = express.Router()
-
-
 export const getItems = async(req,res) => {
 	try{
-		//const { category = 'all'} = req.query
-		
-		//console.log(category)
-		//let items
-		//if(category !== 'all') items = await Item.find({category: category})
-		//if(category === 'all') items = await Item.find()
 		let { category = 'all'} = req.query
 		let items
         let collection = await db.collection("products");
-       // let results = await collection.find({}).limit(50).toArray();
        if(category !== 'all') items = 
                     await collection.find({category: category}).toArray()
 	   if(category === 'all') items = await collection.find({}).toArray()
@@ -34,7 +23,7 @@ export const getItem = async(req, res) => {
 		let query = {_id: new ObjectId(req.params.id)}
 	//console.log(query)
 		const item = await collection.findOne(query)
-		res.status(200).json(item)
+		res.send(item).status(200)
 	}catch(error){res.status(404).json({message: error.message})}
 }
 
@@ -54,13 +43,11 @@ export const createItem = async(req,res)=> {
 }
 export const updateItem = async(req, res)=> {
    try{
-    const query = { _id: new ObjectId(req.params.id) };
-    const updates = {
-    $push:  req.body 
-  };
-
-    let collection = await db.collection("products");
+    const query = { _id: new ObjectId(req.params.id) }
+    const updates = {$set:  req.body }
+    let collection = await db.collection("products")
     let result = await collection.updateOne(query, updates)
+    const item = await collection.findOne(query)
 	/*const {id} = req.params
 	const {title, description, creator, photo, price, category, createdAt} = req.body
     
@@ -69,20 +56,22 @@ export const updateItem = async(req, res)=> {
 
 	const updatedItem = {creator, title, description, photo, category, price, _id: id, createdAt}
     await Item.findByIdAndUpdate(id, updatedItem, {new: true})*/
-    res.send(result).status(201)
+    res.send(item).status(201)
 }catch(error){
 		res.status(409).json({message: error.message})
 	}}
 
 export const deleteItem = async(req,res)=> {
-	//const {id} = req.params
-    const query = { _id: ObjectId(req.params.id) };
+  try{
+    const query = { _id: new ObjectId(req.params.id) };
 
-    const collection = db.collection("posts");
+    const collection = db.collection("products");
     let result = await collection.deleteOne(query)
-	/*if(!mongoose.Types.ObjectId.isValid(id))
-		return res.status(404).send(`No post with id: ${id}`)
-	await Item.findByIdAndRemove(id)*/
-	res.json({message: 'Post deleted', id})
-}
-//export default router
+    const item = await collection.findOne(query)
+    console.log(req.params.id)
+	if(!item)
+	 return res.send({message:`No post with id: ${query._id}`}).status(404)
+	
+	res.send({ message: 'Post deleted', ...result}).status(200)
+}catch(error){res.status(409).json({message: error.message})}
+ }
--- back/ctrls/user.js
+++ back/ctrls/user.js
@@ -1,14 +1,18 @@
 import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
+import db from "../conn.js";
 
-import User from '../models/User.js'
+//import User from '../models/User.js'
 const secret = 'test'
 
 export const signin = async(req,res)=> {
-	const {email, password} = req.body
 
   try{
-	const oldUser = await User.findOne({email})
+	const {email, password} = req.body
+	let collection = await db.collection("users")
+	const oldUser = await collection.findOne({email})
+	console.log(oldUser)
+	//const oldUser = await User.findOne({email})
 	if(!oldUser) return res.status(404).json({message: "User doesn't exist"})
    
 	const isPasswordCorrect = await bcrypt.compare(password, oldUser.password)
@@ -21,20 +25,21 @@ export const signin = async(req,res)=> {
 	}
    }
     export const signup = async (req, res) => {
-  const { name, email, password} = req.body;
+
 
   try {
-    const oldUser = await User.findOne({ email });
-
+	const { name, email, password} = req.body
+    let collection = await db.collection("users")
+    const oldUser = await collection.findOne({ email });
     if (oldUser) return res.status(400).json({ message: "User already exists" });
 
     const hashedPassword = await bcrypt.hash(password, 12);
-
-    const result = await User.create({ email, password: hashedPassword, name: `${name}`, role: 'user' });
+    const newUser = { email, password: hashedPassword, name: `${name}`, role: 'user' }
+    const result = await collection.insertOne(newUser)
 
     const token = jwt.sign( { email: result.email, id: result._id }, secret, { expiresIn: "1h" } );
 
-    res.status(201).json({ result, token });
+    res.status(201).json({ newUser, token });
   } catch (error) {
     res.status(500).json({ message: "Something went wrong" });
     
