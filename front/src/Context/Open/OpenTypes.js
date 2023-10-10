4d5e04299afe661dd316074bfd80c46e47fac20e
Yaroslav Moroz <ya.moroz.yaroslav@gmail.com>
2023-10-10 18:53:58 -0300

    57

--- front/src/Context/Open/OpenTypes.js
+++
@@ -1,8 +0,0 @@
-export const OPEN_ITEM = 'OPEN_ITEM'
-export const CLOSE_ITEM = 'CLOSE_ITEM'
-
-export const OPEN_AUTH = 'OPEN_AUTH'
-export const CLOSE_AUTH = 'CLOSE_AUTH'
-
-export const OPEN_MAIL = 'OPEN_MAIL'
-export const CLOSE_MAIL = 'CLOSE_MAIL'
