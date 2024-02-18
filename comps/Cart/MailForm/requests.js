e72ce1330c7592bcab78b7a1f7db5b8cdfcd1e66
Yaroslav Moroz <80963959+yaMorozYaroslav@users.noreply.github.com>
2024-02-18 19:49:30 -0400

    Update requests.js

--- comps/Cart/MailForm/requests.js
+++ comps/Cart/MailForm/requests.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export const getAreas =()=> axios.post('https://api.novaposhta.ua/v2.0/json/',
                                     {
-                            "apiKey": "eee3a5f0b4d1ba07016827f6dff25e86",
+                            "apiKey": "",
                             "modelName": "Address",
                             "calledMethod": "getSettlements",
                             "methodProperties": {"Ref" : ""}})
