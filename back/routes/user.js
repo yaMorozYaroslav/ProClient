775cbbe0eec39e5e9cd152d98e241ca2a9a1b4be
Yaroslav Moroz <ya.moroz.yaroslav@gmail.com>
2024-04-05 19:44:11 -0300

    977

--- back/routes/user.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-const router = express.Router()
-
-import {signin, signup} from '../ctrls/user.js'
-
-router.post('/signin', signin)
-router.post('/signup', signup)
-
-export default router
