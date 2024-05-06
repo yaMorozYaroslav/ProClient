19999af8983120a760754b98e6bcf9340f66f118
Yaroslav Moroz <ya.moroz.yaroslav@gmail.com>
2024-05-06 09:23:13 -0300

    64

--- app/[locale]/layout.js
+++ app/[locale]/layout.js
@@ -10,13 +10,18 @@ import {GlobalStyle} from './extra.styled'
 import StyledComponentsRegistry from './registry';
 import {Russo_One} from 'next/font/google'
 
-const lora = Russo_One({ subsets: ['cyrillic'], weight: ['400'] })
-//~ import {useLocale} from 'next-intl'
-//~ 
 import {notFound} from 'next/navigation'
 import { NextIntlClientProvider } from 'next-intl'
+import {unstable_setRequestLocale} from 'next-intl/server';
 
-const locales = ['en', 'ua']
+const lora = Russo_One({ subsets: ['cyrillic'], weight: ['400'] })
+//~ import {useLocale} from 'next-intl'
+
+const locales = ['en', 'ua', 'ru']
+
+export function generateStaticParams() {
+  return locales.map((locale) => ({locale}));
+}
 //~ const lora = Lora({ subsets: ['latin'] })
 //~ const lora = Yeseva_One({subsets: ['cyrillic'], weight: '400' })
 export const metadata = {
@@ -36,7 +41,7 @@ let messages
     notFound()
   }
  //~ if (!locales.includes(locale)) notFound();
- 
+   unstable_setRequestLocale(locale)
   return (
   <html lang={locale}>
     <body className={lora.className}>
