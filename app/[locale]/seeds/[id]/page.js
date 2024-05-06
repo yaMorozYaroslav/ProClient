d7443979b403ec36f081cd923158c414969e567a
Yaroslav Moroz <ya.moroz.yaroslav@gmail.com>
2024-05-06 08:51:17 -0300

    63

--- app/[locale]/seeds/[id]/page.js
+++ app/[locale]/seeds/[id]/page.js
@@ -7,7 +7,7 @@ export async function generateStaticParams(){
   const seeds = await fetch(
     'https://seed-shop-back-78049b8c30bb.herokuapp.com/seeds?category=&type=')
                                         .then((res) => res.json())
-  const arrSeeds = seeds.data.map((seed) => ({id: seed._id}))
+  const arrSeeds = seeds.data.map((seed) => ({id: seed._id, locale: 'ua'}))
     return  arrSeeds
 	}
 	
